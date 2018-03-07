1e4768e5994b7ceeda2805bd87d3c1c9ad821a96
Estudiantes <Estudiantes@Labing70201>
2018-03-07 17:52:46 -0500

    segundo commit

--- Buscaminas/src/buscaminas/Sembrador.java
+++ Buscaminas/src/buscaminas/Sembrador.java
@@ -5,22 +5,49 @@
  */
 package buscaminas;
 
+import java.util.Random;
 
 public class Sembrador {
-    
-    
-    
-    
-void sembrarCorazon (Celda lasCeldas [][], int cantidad){
-    
-    for (Celda [] laFila:lasCeldas){
-        
-        for(Celda laCelda:laFila){
-            Corazon unCorazon=new Corazon ();
-            laCelda.setMiCorazon(unCorazon);
+
+    void sembrarCorazon(Celda lasCeldas[][], int cantidad) {
+
+        Random aleatorio;
+        aleatorio = new Random();
+        int corazonAleatorio;
+        int sumatoria = 0;
+
+        for (Celda[] laFila : lasCeldas) {
+
+            for (Celda laCelda : laFila) {
+
+                corazonAleatorio = aleatorio.nextInt(10);
+                if (corazonAleatorio > 5 && sumatoria < cantidad) {
+
+                    Corazon unCorazon = new Corazon();
+                    laCelda.setMiCorazon(unCorazon);
+                    sumatoria++;
+                }
+            }
         }
-    }
+        
+
     
     
-}    
+    int x,y;
+      
+       int contador =0;
+       for(x=-1; x<=2; x++){
+           for (y=-1;y <=2; y++){
+               if (laCelda[laFila+x][columna+y].getCorazon ()!= null){
+                 contador ++;  
+               }
+           
+          
+       }
+       }
+        
+     } 
+    
+    
 }
+
